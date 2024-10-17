62b2f02b214cb20801c5d9076e15bae3301ef710
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-17 14:25:15 +0200

    Dice Game → Variable Init

--- js/main.js
+++ js/main.js
@@ -3,18 +3,27 @@
 // # controlla che sia nella lista di chi può accedere,
 // # stampa un messaggio appropriato sull'esito del controllo.
 
-const emails = [
-  "me@email.it",
-  "other@email.com",
-  "manz@mail.it",
-  "gov@email.gov",
-];
-const userEmail = prompt("Inserisci l'Email");
+// const emails = [
+//   "me@email.it",
+//   "other@email.com",
+//   "manz@mail.it",
+//   "gov@email.gov",
+// ];
+// const userEmail = prompt("Inserisci l'Email");
 
-emails.includes(userEmail)
-  ? console.log("Email presente nel DataBase")
-  : console.log("Email non presente nel DataBase");
+// emails.includes(userEmail)
+//   ? console.log("Email presente nel DataBase")
+//   : console.log("Email non presente nel DataBase");
 
 // ! Gioco dei dadi
 // # Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // # Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const maxRandomNumber = 6;
+const minRandomNumber = 1;
+const userRandomNumber = Math.floor(
+  Math.random() * maxRandomNumber + minRandomNumber
+);
+const computerRandomNumber = Math.floor(
+  Math.random() * maxRandomNumber + minRandomNumber
+);
