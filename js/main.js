171da2b29f64ab37af609378b247befafd566091
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-17 13:56:11 +0200

    Mail → Variable Init

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-Mail
-Chiedi all'utente la sua email,
-controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull'esito del controllo.
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// ! Mail
+// # Chiedi all'utente la sua email,
+// # controlla che sia nella lista di chi può accedere,
+// # stampa un messaggio appropriato sull'esito del controllo.
+
+const emails = [
+  "me@email.it",
+  "other@email.com",
+  "manz@mail.it",
+  "gov@email.gov",
+];
+const userEmail = prompt("Inserisci l'Email");
+let outputTextS;
+
+// ! Gioco dei dadi
+// # Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// # Stabilire il vincitore, in base a chi fa il punteggio più alto.
